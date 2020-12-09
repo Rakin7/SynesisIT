6e9482c65a84e9be14d3ae81aaec0256e2c967a9
Rakin Bin Rabbani <35832569+Rakin7@users.noreply.github.com>
2020-12-09 17:20:50 +0600

    Add files via upload

---
+++ WebAPI/WebAPIStart/WebAPIStart/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIStart.Models;
+
+namespace WebAPIStart.Controllers
+{
+    public class UserController : ApiController
+    {
+        List<User> userList = new List<User>();
+        public UserController()
+        {
+            userList.Add(new User { UserId = 1, Name = "Rakin", Address = "Dhaka" });
+            userList.Add(new User { UserId = 2, Name = "Minhaz", Address = "Feni" });
+        }
+        //api/user
+        public IHttpActionResult GetUsers()
+        {
+            return Ok(userList);
+        }
+        //api/user/1
+        public IHttpActionResult GetUser(int id)
+        {
+            User user = userList.Find(u => u.UserId == id);
+            return Ok(user);
+        }
+    }
+}
